aee327ca4e2b25506cf05c34e1cc90e6f879fb1d
Daniel Vicent <daniel.vicent.luna@gmail.com>
2024-07-02 19:25:26 +0200

    MUI installed and color palette created

---
+++ src/assets/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material"
+
+export const theme = createTheme({
+    palette: {
+        mainGreen: '#104547',
+        pearl: '#e3dbbe',
+        mainBrown: '#563635',
+        platinum: '#E0E0E0'
+    }
+})
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ThemeProvider } from '@mui/material/styles';
 import './index.css'
+import { theme } from './assets/theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
